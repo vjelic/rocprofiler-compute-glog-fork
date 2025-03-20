1085dd0681de6c3e022ffc9cd521e6c31c522fa3
benrichard-amd <ben.richard@amd.com>
2025-03-20 11:23:13 -0400

    Fix formatting

--- src/rocprof_compute_analyze/analysis_webui.py
+++ src/rocprof_compute_analyze/analysis_webui.py
@@ -342,9 +342,7 @@ class webui_analysis(OmniAnalyze_Base):
             self._arch_configs[self.arch],
         )
         if args.random_port:
-            self.app.run(
-                debug=False, host="0.0.0.0", port=random.randint(1024, 49151)
-            )
+            self.app.run(debug=False, host="0.0.0.0", port=random.randint(1024, 49151))
         else:
             self.app.run(debug=False, host="0.0.0.0", port=args.gui)
 
